425f080663c2ccddc9468321c1a5face37933723
andreea <andreeagorgoi@gmail.com>
2020-10-25 21:45:12 +0200

    day 4 exercises

--- app/src/main/java/com/example/internshipexercises/MainActivity.java
+++ app/src/main/java/com/example/internshipexercises/MainActivity.java
@@ -1,65 +1,50 @@
 package com.example.internshipexercises;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import com.example.internshipexercises.Utils.DownloaderUtil;
 
-import com.example.internshipexercises.model.Post;
-import com.example.internshipexercises.server.ServerProvider;
-
-import java.io.IOException;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    ImageView imageView;
+    Runnable downloadRunnable = new DownloadRunnable();
+
+    class DownloadRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            Bitmap bitmap = DownloaderUtil.INSTANCE.downloadImage();
+            runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+        }
+    }
+
+    private void downloadImageUsingExecutor() {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.submit(downloadRunnable);
+    }
+
+    private void downloadImageUsingThread(){
+        new Thread(() -> {
+            final Bitmap bitmap = DownloaderUtil.INSTANCE.downloadImage();
+            runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+        }).start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        getPostSynchronously();
-        getPostASynchronously();
-
-    }
-
-    private void getPostASynchronously() {
-        ServerProvider.createPostService().getPosts().enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                List<Post> posts = response.body();
-                if(posts!=null){
-                    Log.d(TAG, "there are" + posts.size() );
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-                Log.e(TAG, "error");
-            }
-        });
-    }
-
-    private void getPostSynchronously() {
-        try {
-            Response<List<Post>> response = ServerProvider.createPostService().getPosts().execute();
-            List<Post> posts = response.body();
-            if(posts!=null){
-                Log.d(TAG, "there are" + posts.size() );
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "error");
-        }
+        imageView = findViewById(R.id.image_view);
+//        downloadImageUsingThread();
+        downloadImageUsingExecutor();
     }
 
 }
