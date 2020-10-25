b1ac23faf5b19e3642f169a12e79e51a39778b44
andreea <andreeagorgoi@gmail.com>
2020-10-25 20:48:18 +0200

    day 4

--- app/src/main/java/com/example/internshipexercises/MainActivity.java
+++ app/src/main/java/com/example/internshipexercises/MainActivity.java
@@ -8,75 +8,58 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.example.internshipexercises.model.Post;
+import com.example.internshipexercises.server.ServerProvider;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
-    private int incrementValue = 0;
-    private TextView incrementTv;
-    private Button incrementBtn;
-    private TextView textView;
-
-    private void initViews(){
-        incrementTv = findViewById(R.id.counter_value_tv);
-        incrementBtn = findViewById(R.id.increment_bt);
-
-        incrementTv.setText(String.valueOf(incrementValue));
-        incrementBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                incrementValue++;
-                incrementTv.setText(String.valueOf(incrementValue));
-            }
-        });
-    }
-
-
-    protected void onSaveInstanceState(Bundle state){
-        super.onSaveInstanceState(state);
-        state.putInt("nrOfTimes", incrementValue);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Happy to be born");
 
+//        getPostSynchronously();
+        getPostASynchronously();
 
-        if((savedInstanceState != null) && (savedInstanceState.getSerializable("nrOfTimes") != null)){
-            incrementValue = savedInstanceState.getInt("nrOfTimes");
-        }else{
-            incrementValue = 0;
+    }
+
+    private void getPostASynchronously() {
+        ServerProvider.createPostService().getPosts().enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                List<Post> posts = response.body();
+                if(posts!=null){
+                    Log.d(TAG, "there are" + posts.size() );
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.e(TAG, "error");
+            }
+        });
+    }
+
+    private void getPostSynchronously() {
+        try {
+            Response<List<Post>> response = ServerProvider.createPostService().getPosts().execute();
+            List<Post> posts = response.body();
+            if(posts!=null){
+                Log.d(TAG, "there are" + posts.size() );
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "error");
         }
-
-        initViews();
     }
 
-    protected void onStart(){
-        super.onStart();
-        Log.d(TAG, "onStart: I exist but you cannot see me");
-    }
-
-    protected void onResume(){
-        super.onResume();
-        Log.d(TAG, "onResume: Preparing final UI changes for master");
-    }
-
-    protected void onPause(){
-        super.onPause();
-        Log.d(TAG, "onPause: You can see me, but i don t want to interact");
-    }
-
-    protected void onStop(){
-        super.onStop();
-        Log.d(TAG, "onStop: Packing up, preparing to leave");
-    }
-
-
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: Bye bye");
-    }
 }
---
+++ app/src/main/java/com/example/internshipexercises/server/ServerProvider.java
@@ -0,0 +1,30 @@
+package com.example.internshipexercises.server;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServerProvider {
+
+    public final static String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    public static PostService createPostService() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient =
+                new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        return retrofit.create(PostService.class);
+    }
+
+    private ServerProvider(){};
+}
