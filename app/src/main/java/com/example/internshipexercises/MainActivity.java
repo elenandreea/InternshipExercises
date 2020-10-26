6de9f69e069bde16f8f1a205d5d8a984ecdca490
andreea <andreeagorgoi@gmail.com>
2020-10-26 11:11:14 +0200

    day 5 design

--- app/src/main/java/com/example/internshipexercises/MainActivity.java
+++ app/src/main/java/com/example/internshipexercises/MainActivity.java
@@ -1,40 +1,61 @@
 package com.example.internshipexercises;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.appcompat.widget.Toolbar;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+//Details activity
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapView mapView = findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-        mapView.onResume();
+        setToolbar();
+    }
 
-        mapView.getMapAsync(this);
+    private void setToolbar() {
+        Toolbar mytoolbar = findViewById(R.id.my_toolbar);
+        mytoolbar.setTitle("Restaurant name");
+        mytoolbar.setSubtitle("Restaurant description");
+        setSupportActionBar(mytoolbar);
 
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng coord = new LatLng(46.7676919, 23.5709693);
-        googleMap.addMarker(new MarkerOptions().position(coord));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coord,16));
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
+    }
 
-        googleMap.addMarker(new MarkerOptions().position(coord).title("Porto").snippet("Welcome to Porto, Portugal!"));
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_favourite:
+                Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
